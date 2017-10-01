87ffd668493ba458d6c79a9a33d28cece177c6cb
Nikita Marshalkin <marnikitta@gmail.com>
2017-10-01 12:11:41 +0300

    Create http-frontend template

--- frontend/src/main/java/marnikitta/concierge/frontend/ConciergeApplication.java
+++ frontend/src/main/java/marnikitta/concierge/frontend/ConciergeApplication.java
@@ -1,7 +1,11 @@
 package marnikitta.concierge.frontend;
 
 import akka.NotUsed;
+import akka.actor.ActorPath;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Address;
+import akka.actor.RootActorPath;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -11,30 +15,60 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import com.typesafe.config.ConfigFactory;
+import marnikitta.concierge.common.Cluster;
+import marnikitta.concierge.kv.LinearizableStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.net.InetAddress;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletionStage;
 
 public final class ConciergeApplication extends AllDirectives {
-  public ConciergeApplication() {
+  private final Logger LOG = LoggerFactory.getLogger(ConciergeApplication.class);
+
+  private final Cluster cluster;
+
+  private ActorRef kv;
+
+  public ConciergeApplication(Cluster cluster) {
+    this.cluster = cluster;
   }
 
   public static void main(String... args) throws IOException {
-    new ConciergeApplication().run();
+    final Set<ActorPath> clusterPaths = new HashSet<>();
+
+    final List<String> localString = Collections.singletonList("localhost:23456");
+
+    for (String arg : localString) {
+      final String[] split = arg.split(":");
+      final InetAddress host = InetAddress.getByName(split[0]);
+      final int port = Integer.parseInt(split[1]);
+
+      final Address actorSystemAddress = new Address("akka.tcp", "concierge", host.getHostName(), port);
+      clusterPaths.add(RootActorPath.apply(actorSystemAddress, "/").child("user"));
+    }
+
+    new ConciergeApplication(new Cluster(clusterPaths)).run();
   }
 
   public void run() throws IOException {
     final ConnectHttp host = ConnectHttp.toHost("localhost", 8080);
 
-    final ActorSystem system = ActorSystem.create();
+    final ActorSystem system = ActorSystem.create("concierge", ConfigFactory.load("remote"));
+    kv = system.actorOf(LinearizableStorage.props(new Cluster(cluster.paths(), "kv")), "kv");
+
     final ActorMaterializer materializer = ActorMaterializer.create(system);
 
     final Flow<HttpRequest, HttpResponse, NotUsed> theFlow = createRoute().flow(system, materializer);
-    final CompletionStage<ServerBinding> binding = Http.get(system)
-            .bindAndHandle(theFlow, host, materializer);
+    final CompletionStage<ServerBinding> binding = Http.get(system).bindAndHandle(theFlow, host, materializer);
 
-    System.out.println("Ama up");
+    LOG.info("Ama up");
     System.in.read();
 
     binding.thenCompose(ServerBinding::unbind)
@@ -42,6 +76,6 @@ public final class ConciergeApplication extends AllDirectives {
   }
 
   public Route createRoute() {
-    return route();
+    return route(complete("pong"));
   }
 }
