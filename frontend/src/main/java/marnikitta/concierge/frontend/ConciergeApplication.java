1c7efc573e62e4e73874fb53298e9e2de36f329c
Nikita Marshalkin <marnikitta@gmail.com>
2017-10-04 21:01:34 +0300

    Implement session service draft

--- frontend/src/main/java/marnikitta/concierge/frontend/ConciergeApplication.java
+++ frontend/src/main/java/marnikitta/concierge/frontend/ConciergeApplication.java
@@ -9,17 +9,26 @@ import akka.actor.RootActorPath;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.http.javadsl.server.directives.RouteAdapter;
+import akka.pattern.PatternsCS;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.typesafe.config.ConfigFactory;
 import marnikitta.concierge.common.Cluster;
 import marnikitta.concierge.kv.LinearizableStorage;
+import marnikitta.concierge.kv.session.SessionAPI;
+import marnikitta.concierge.model.ConciergeFailure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.Function1;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -29,8 +38,13 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletionStage;
 
+import static akka.http.javadsl.server.PathMatchers.longSegment;
+import static scala.compat.java8.JFunction.func;
+
 public final class ConciergeApplication extends AllDirectives {
   private final Logger LOG = LoggerFactory.getLogger(ConciergeApplication.class);
+  private final ObjectMapper mapper = new ObjectMapper()
+          .registerModule(new JavaTimeModule());
 
   private final Cluster cluster;
 
@@ -76,6 +90,37 @@ public final class ConciergeApplication extends AllDirectives {
   }
 
   public Route createRoute() {
-    return route(complete("pong"));
+    final Function1<Object, RouteAdapter> expectedFailureMapper = func(response -> {
+      if (response instanceof ConciergeFailure) {
+        return complete(
+                StatusCodes.IM_A_TEAPOT,
+                ((ConciergeFailure) response).message(),
+                Jackson.marshaller(mapper)
+
+        );
+      } else {
+        return complete(StatusCodes.OK, response, Jackson.marshaller(mapper));
+      }
+    });
+
+    final Route sessionPath = route(
+            put(() ->
+                    path(longSegment(), id ->
+                            onComplete(
+                                    PatternsCS.ask(kv, new SessionAPI.CreateSession(id), 100),
+                                    sessionTry -> sessionTry.map(expectedFailureMapper).get()
+                            )
+                    )
+            ),
+            patch(() ->
+                    path(longSegment(), id ->
+                            onComplete(
+                                    PatternsCS.ask(kv, new SessionAPI.Heartbeat(id), 100),
+                                    sessionTry -> sessionTry.map(expectedFailureMapper).get()
+                            )
+                    )
+            )
+    );
+    return route(sessionPath);
   }
 }
